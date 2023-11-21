c6bb638cd8745af0f76e35e6341d8ed34094221e
Anant Agarwal <anantagarwal44@gmail.com>
2023-11-21 21:08:08 +0530

    Update proto_v1.py

--- proto_v1.py
+++ proto_v1.py
@@ -69,6 +69,8 @@ def eps_valuation(ticker, ttm_eps, growth_rate, growth_decline_rate):
         #st.write("EPS in Y", year+1, ":", final_eps_val)
     
     # st.write(yf.Ticker(ticker).info)
+    st.write("Get Forward P/E")
+    st.write(get_forward_pe_from_website(ticker))
 
     #Projected Value
     final_eps_val = final_eps_val * (get_forward_pe_from_website(ticker))
